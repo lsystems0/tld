884edf74c25999174545abface60216ce175ed7c
luser0 <merooooyasser@gmail.com>
2026-02-07 01:00:17 +0200

    fix: yellow

--- src/components/ui/WeDontRush.tsx
+++ src/components/ui/WeDontRush.tsx
@@ -2,12 +2,12 @@ export function WeDontRush() {
   return (
     <div className="relative mx-6 px-0 py-0 md:px-6 md:py-5">
       {/* Top right corner */}
-      <div className="absolute -top-2 -right-2 h-2 w-6 bg-yellow-400 md:h-3 md:w-8" />
-      <div className="absolute -top-2 -right-2 h-6 w-2 bg-yellow-400 md:h-8 md:w-3" />
+      <div className="bg-tld-yellow absolute -top-2 -right-2 h-2 w-6 md:h-3 md:w-8" />
+      <div className="bg-tld-yellow absolute -top-2 -right-2 h-6 w-2 md:h-8 md:w-3" />
 
       {/* Bottom left corner */}
-      <div className="absolute -bottom-2 -left-2 h-2 w-6 bg-yellow-400 md:h-3 md:w-8" />
-      <div className="absolute -bottom-2 -left-2 h-6 w-2 bg-yellow-400 md:h-8 md:w-3" />
+      <div className="bg-tld-yellow absolute -bottom-2 -left-2 h-2 w-6 md:h-3 md:w-8" />
+      <div className="bg-tld-yellow absolute -bottom-2 -left-2 h-6 w-2 md:h-8 md:w-3" />
 
       <div>
         <p className="z-1 px-4 text-lg whitespace-nowrap md:text-2xl">
